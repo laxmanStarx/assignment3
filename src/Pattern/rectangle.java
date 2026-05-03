2cb205d902dac591e429288c1f3159a5808431d7
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-05-03 23:46:38 +0530

    java1maysk

---
+++ src/Pattern/rectangle.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class rectangle {
+    public static void main(String[] args) {
+        int n =  6;
+        for (int i = 1; i <= n/2; i++) {
+            for(int j = 1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+
+
+
+        }
+
+    }
+}
