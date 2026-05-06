d94e1d2fd40b2cc516fd60dadc2cf64d65b65200
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-05-06 23:55:21 +0530

    jaljeeraklolkllj

---
+++ src/Arrays/arrayInput.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class arrayInput {
+    public static void main(String[] args){
+        int arr[] = new int[5];
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = arr.length;
+
+        for(int i  = 0; i<=n-1;i++){
+            System.out.println("Prvoide input for index"+ i);
+            arr[i] = sc.nextInt();
+
+        }
+        for(int val: arr){
+            System.out.println(val);
+        }
+    }
+}
