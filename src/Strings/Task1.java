b8f278d393c506b992f6fc8c1ab239d7522d5185
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-05-08 00:00:12 +0530

    maysu

--- src/Strings/Task1.java
+++ src/Strings/Task1.java
@@ -4,5 +4,10 @@ public class Task1 {
     public static void main(String[] args){
         String greeting = "Hello";
         System.out.println("Length of string is: "+ greeting.length());
+
+        String txt = "ChickenBiryani";
+        String Movie = "HaRRyPOTTER";
+        System.out.println(txt.toUpperCase());
+        System.out.println(Movie.toLowerCase());
     }
 }
